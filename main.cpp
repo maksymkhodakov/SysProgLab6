13db81717737bdfbdd6fdc9caf7281c60ea29805
jovakinn <max05012004@gmail.com>
2023-11-15 01:26:29 +0200

    add flame graph

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 1) commands to get a flame graph
+// git clone https://github.com/brendangregg/FlameGraph
+// perf record -F 50 --call-graph dwarf ./my_prog arg1 arg2
+// perf script | /home/jovakinn/CLionProjects/flame/FlameGraph/stackcollapse-perf.pl | /home/jovakinn/CLionProjects/flame/FlameGraph/stackcollapse-recursive.pl | /home/jovakinn/CLionProjects/flame/FlameGraph/flamegraph.pl > outMain.svg
+
+
 // Function to return gcd of a and b
 int gcd(int a, int b) {
 
