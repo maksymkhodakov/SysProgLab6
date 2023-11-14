d070259e4f2e5ed9f87299ee4f87176d53541f78
jovakinn <max05012004@gmail.com>
2023-11-15 00:10:26 +0200

    add optimized gcd

--- optimized.cpp
+++ optimized.cpp
@@ -1,30 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to return gcd of a and b
+// Function to return gcd of a and b using the Euclidean algorithm
 int gcd(int a, int b) {
-
-    // Find Minimum of a and b
-    int result = min(a, b);
-    while (result > 0) {
-        if (a % result == 0 && b % result == 0) {
-            break;
-        }
-        result--;
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
     }
-
-    // Return gcd of a and b
-    return result;
+    return a;
 }
 
 // Driver code
 int main() {
-
     for (int a = 100; a < 1500; a++) {
         for (int b = 1500; b >= 1000; b--) {
             cout << "GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;
         }
     }
-
     return 0;
 }
